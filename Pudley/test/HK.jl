4b8ef9b22b6afa146706d8a9fcee5c8224d376f7
Marcelo Veloso Maciel <mvmaciel@fitec.org.br>
2019-12-20 09:21:14 -0300

    better design

--- Pudley/test/HK.jl
+++ Pudley/test/HK.jl
@@ -1,12 +1,13 @@
 import Pkg
+
+Pkg.activate("./Pudley")
+Pkg.instantiate()
+Pkg.precompile()
+
 using Agents
-using Distributions
-using LightGraphs
-using BenchmarkTools
-using Transducers
 using DataVoyager
 
-mutable struct HKAgent{T<:AbstractFloat} <: AbstractAgent
+mutable struct HKAgent{T <: AbstractFloat} <: AbstractAgent
     id::Int
     old_opinion::T
     new_opinion::T
@@ -14,49 +15,47 @@ end
 
 myscheduler(m) = keys(m.agents)
 
-function hk_model(;numagents = 100, ϵ=0.3)
-    model = ABM(HKAgent, scheduler=myscheduler , properties = Dict(:ϵ => ϵ))
+function hk_model(;numagents = 100, ϵ = 0.4)
+    model = ABM(HKAgent, scheduler = myscheduler, properties = Dict(:ϵ => ϵ))
     for i in 1:numagents
-        add_agent!(model, rand(), 0.0)
+        o = rand()
+        add_agent!(model, o, o)
     end
     return model
 end
 
+get_old_opinion(agent)::Float64 = agent.old_opinion
 
-get_old_opinion(agent)::Float64= agent.old_opinion
-
-# function agent_step!(agent, model)
-#     condition(j)= abs(get_old_opinion(agent) - get_old_opinion(j)) ≤  model.properties[:ϵ]
-#     xfopinion = (Filter( condition ) |> Map(get_old_opinion))
-#     xfbool = Keep(condition)
-#     meanopinion = reduce(+, xfopinion, values(model.agents)) / reduce(+, xfbool, values(model.agents))
-#     return(meanopinion) #agent.new_opinion = 
-# end
+function boundfilter(agent,model) 
+    filter(j->abs(get_old_opinion(agent) - j) < model.properties[:ϵ],
+     get_old_opinion.(values(model.agents)))
+end
 
 function agent_step!(agent, model)
-    agent.new_opinion =  mean(
-        filter(j -> abs(get_old_opinion(agent) - j) < model.properties[:ϵ], 
-          get_old_opinion.(values(model.agents))))
+    agent.new_opinion = mean(boundfilter(agent,model))
 end
 
 function model_step!(model)
     for i in keys(model.agents)
-        model.agents[i].old_opinion = model.agents[i].new_opinion
+        agent = id2agent(i, model)
+        agent.old_opinion = agent.new_opinion
     end
 end
 
+function model_run(; numagents = 100, iterations = 50, ϵ= 0.3)
+    model = hk_model(numagents = numagents, ϵ = ϵ)
+    when = 0:5:iterations
+    agent_properties = [:new_opinion]
+    data = step!(
+            model,
+            agent_step!, 
+            model_step!,
+            iterations, 
+            agent_properties,
+            when = when
+            ) 
+    return(data)
+end
 
-model = hk_model(numagents = 1000, ϵ=0.1)
-when = map(i -> floor(Int, i),
-           collect(range(0,step= 10,stop = 100)))
-
-agent_properties = [:new_opinion]
-data = step!(model,
-             agent_step!,model_step!,
-              1_000, agent_properties, when = when) 
-
-
-
+data = model_run()
 v = Voyager(data)
-
-
