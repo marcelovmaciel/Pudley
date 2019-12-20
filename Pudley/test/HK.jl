24062f96d044e3cdce8122cc58a3b19bc306a805
Marcelo Veloso Maciel <mvmaciel@fitec.org.br>
2019-12-20 09:21:14 -0300

    cleaned

--- Pudley/test/HK.jl
+++ Pudley/test/HK.jl
@@ -1,9 +1,11 @@
 import Pkg
+
+
+Pkg.activate("./Pudley")
+Pkg.instantiate()
+Pkg.precompile()
+
 using Agents
-using Distributions
-using LightGraphs
-using BenchmarkTools
-using Transducers
 using DataVoyager
 
 mutable struct HKAgent{T<:AbstractFloat} <: AbstractAgent
@@ -22,16 +24,7 @@ function hk_model(;numagents = 100, ϵ=0.3)
     return model
 end
 
-
 get_old_opinion(agent)::Float64= agent.old_opinion
-
-# function agent_step!(agent, model)
-#     condition(j)= abs(get_old_opinion(agent) - get_old_opinion(j)) ≤  model.properties[:ϵ]
-#     xfopinion = (Filter( condition ) |> Map(get_old_opinion))
-#     xfbool = Keep(condition)
-#     meanopinion = reduce(+, xfopinion, values(model.agents)) / reduce(+, xfbool, values(model.agents))
-#     return(meanopinion) #agent.new_opinion = 
-# end
 
 function agent_step!(agent, model)
     agent.new_opinion =  mean(
@@ -45,18 +38,12 @@ function model_step!(model)
     end
 end
 
-
 model = hk_model(numagents = 1000, ϵ=0.1)
 when = map(i -> floor(Int, i),
            collect(range(0,step= 10,stop = 100)))
-
 agent_properties = [:new_opinion]
 data = step!(model,
              agent_step!,model_step!,
               1_000, agent_properties, when = when) 
 
-
-
 v = Voyager(data)
-
-
