92a06ac95ef74cda85943df4773b75f1924fac5e
mvmaciel <marcelovmaciel@protonmail.com>
2020-02-06 16:38:26 -0300

    a

--- Pudley/test/01_testpop2.jl
+++ Pudley/test/01_testpop2.jl
@@ -1,6 +1,6 @@
 import Pkg
 
-Pkg.activate("../../Pudley")
+Pkg.activate("Pudley")
 Pkg.instantiate()
 Pkg.precompile()
 
@@ -8,40 +8,69 @@ import Revise
 import Pudley
 const pdl = Pudley
 using BenchmarkTools
-using DataVoyager,
-    VegaLite, ElectronDisplay
+# using DataVoyager,
+#     VegaLite, ElectronDisplay
 import Base.Filesystem
 const filesystem = Base.Filesystem
+using Plots
 
-m = pdl.model_initialize(n=2)
+n = 20
+m = pdl.model_initialize(n=n)
 
-n = 100_000
+t = 1000
 
 agent_properties = [:o , :σ]
 
 when = map(i -> floor(Int, i),
-           collect(range(0,step= 1000,stop = n)))
+           collect(range(0,step= 10,stop = t)))
 
 when[1] = 1
 
 data = pdl.Abm.step!(m,
               pdl.Abm.dummystep,
               pdl.pudley_step!,
-              n, agent_properties, when = when)     # run the model one step
+              t, agent_properties, when = when)     # run the model one step
 
-v = Voyager(data)
+# v = Voyager(data)
+
+p1 = plot(
+    data[!, :step],
+    data[!, :o],
+    group = data[!, :id],
+    alpha = 0.5,
+    line=4,
+    marker=([:hex :d]),
+    title= "opinion"
+    )
+
+
+
+ p2 = plot(
+        data[!, :step],
+        data[!, :σ],
+        group = data[!, :id],
+        alpha = 0.5,
+        line=4,
+        marker=([:hex :d]),
+        title= "sigma"
+         )
+    
+plot(p1,p2, layout = (1,2))
+savefig("plot-n($n).png")
 print(data)
-plts = ("img" |>
-        filesystem.readdir .|>
-        x -> filesystem.joinpath("./img", x))
 
-function pltfile(plt)
-    if occursin(".vegalite", plt)
-        data |> load(plt) |> save("$(split(plt, "vegalite")[1])png")
-    end
-end
 
-pltfile.(plts)
+# plts = ("img" |>
+#         filesystem.readdir .|>
+#         x -> filesystem.joinpath("./img", x))
+
+# function pltfile(plt)
+#     if occursin(".vegalite", plt)
+#         data |> load(plt) |> save("$(split(plt, "vegalite")[1])png")
+#     end
+# end
+
+# pltfile.(plts)
 
 # data |> @vlplot(
 #     mark={
@@ -50,9 +79,8 @@ pltfile.(plts)
 #     },
 #     x=:step,
 #     y=:o,
-#     color="id:n", legend =
+#     color="id:n"
 # )
-
 
 # ax = lineplot(x="step", y="o", hue="id",
 #                    data=data)
